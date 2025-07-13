fadad1b05ede1a069ea78aa2d2b657db3d6c7aca
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2025-07-13 13:46:11 +0200

    First commit: setup MelonMod Rider project boilerplate.
    * Add a .gitignore for artefacts and assemblies.
    * Add a assemblies/ dir to locally host the dependency assemblies.
    * Configure .csproj to specify the chosen Assembly dependencies as References.
    * Add a Hello World boilerplate MelonLoader MelonMod in KCUI.cs.
    * Configure Properties/Assemblyinfo.cs for the MelonMod.

---
+++ KCUI/KCUI.cs
@@ -0,0 +1,12 @@
+using MelonLoader;
+
+namespace KCUI
+{
+    public class KCUI : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            LoggerInstance.Msg("\nHello World! Hello World! Hello World!\n");
+        }
+    }
+}
---
+++ KCUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using MelonLoader;
+using KCUI; // The mod class namespace.
+
+// MelonLoader config.
+[assembly: MelonInfo(typeof(KCUI.KCUI), "KCUI", "1.0.0", "ELthomaso")]
+[assembly: MelonGame("Raw Fury", "Kingdom: Classic")]
+
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KCUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KCUI")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("AB8090D0-6AEC-4907-97BF-FBEBFAFA0743")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
