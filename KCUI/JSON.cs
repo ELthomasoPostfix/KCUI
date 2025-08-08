4f1854d7563b75363e45373cedb0c57216ece235
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2025-08-08 14:44:07 +0200

    Add Newtonsoft.Json dependency to dump Days & Waves to json.
    * Replace 'json' functions that converted Kingdom
     objects to json by dataclasses/structs that contain
     the to-dump data.
    * Include Newtonsoft.Json as dependency.
    * Use Newtonsoft.Json in KCUI.cs to dump json
     of Wave and Day objects.

--- KCUI/JSON.cs
+++ KCUI/JSON.cs
@@ -1,90 +1,89 @@
 using System;
-using System.IO;
 using MelonLoader.Assertions;
 using UnityEngine;
-using UnityEngine.Assertions;
 
+// A collection of structs that simplify Kingdom objects for dumping to JSON.
 namespace KCUI
 {
-    public class JSON
+    [System.Serializable]
+    public struct DumpDay
     {
-        public static string json(Day day)
+        public string Name;
+        public string Type;
+        public string NotesGeneral;
+        public string NotesSeason;
+
+        public DumpDay(Day day)
         {
-            // The Day notes are formatted as a bullet list with '-' as bullets.
-            // The first bullet specifies a general description, the second hints
-            // at a fitting season.
             string[] notes = day.notes.Replace("\n", "").Replace("\r", "").Split('-');
-            string notesGeneral = notes.Length >= 2 ? notes[1] : "";
-            string notesSeason = notes.Length >= 3 ? notes[2] : "";
-            return $"\"{day.name}\": {{\n\"type\": \"{day.wave.name}\",\n\"notes-general\": \"{notesGeneral}\",\n\"notes-season\": \"{notesSeason}\"\n}}";
+            this.Name=day.name;
+            this.Type=day.wave.name;
+            this.NotesGeneral = notes.Length >= 2 ? notes[1] : "";
+            this.NotesSeason = notes.Length >= 3 ? notes[2] : "";
         }
+    }
 
-        public static string json(Day[] days)
-        {
-            string s = "";
-            foreach (Day day in days)
-                s += json(day) + ",\n";
-            return s;
-        }
+    [System.Serializable]
+    public struct DumpCurve
+    {
+        public float[] Times;
+        public float[] Values;
+        public float[] TimesSampled;
+        public float[] ValuesSampled;
 
-        public static string json(Wave wave)
+        public DumpCurve(AnimationCurve curve)
         {
-            string s = $"\"{wave.name}\": {{\n";
-            s += $"\"numTrolls\": {json(wave.numTrolls)},\n";
-            s += $"\"numToughTrolls\": {json(wave.numToughTrolls)},\n";
-            s += $"\"numBosses\": {json(wave.numBosses)},\n";
-            s += $"\"numKillerBosses\": {json(wave.numKillerBosses)},\n";
-            s += $"\"numOgres\": {json(wave.numOgres)},\n";
-            s += $"\"numSquids\": {json(wave.numSquids)},\n";
-            s += "}";
-            return s;
-        }
-
-        public static string json(AnimationCurve curve)
-        {
-            string s = "{\n";
-            float[] times = new float[curve.keys.Length];
-            float[] values = new float[curve.keys.Length];
+            this.Times = new float[curve.keys.Length];
+            this.Values = new float[curve.keys.Length];
             for (int i = 0; i < curve.keys.Length; i++)
             {
-                times[i] =  curve.keys[i].time;
-                values[i] = curve.keys[i].value;
+                this.Times[i] =  curve.keys[i].time;
+                this.Values[i] = curve.keys[i].value;
             }
-            s += $"\"times\": {json(times)},\n";
-            s += $"\"values\": {json(values)},\n";
 
-            // Sample points to draw the curve more clearly.
+            // Sample points to draw the curve less jaggedly.
+            this.TimesSampled  = new float[0];
+            this.ValuesSampled = new float[0];
             if (curve.keys.Length > 0)
             {
                 int lo = (int)Math.Floor(curve.keys[0].time);
                 int hi = (int)Math.Ceiling(curve.keys[curve.keys.Length - 1].time);
-                float[] timesSampled = new float[hi-lo+1];
-                float[] valuesSampled = new float[hi-lo+1];
+                this.TimesSampled = new float[hi-lo+1];
+                this.ValuesSampled = new float[hi-lo+1];
                 // Only consider the integer time values, because Kingdom samples
-                // the curves for integer day numbers.
+                // the curves for integer day numbers as time.
                 for (int time = lo; time <= hi; time++)
                 {
-                    timesSampled[time-lo] = time;
-                    valuesSampled[time-lo] = curve.Evaluate(time);
+                    this.TimesSampled[time-lo] = time;
+                    this.ValuesSampled[time-lo] = curve.Evaluate(time);
                 }
-                LemonAssert.IsEqual(lo, timesSampled[0]);
-                LemonAssert.IsEqual(hi, timesSampled[timesSampled.Length - 1]);
-                s += $"\"times-sampled\": {json(timesSampled)},\n";
-                s += $"\"values-sampled\": {json(valuesSampled)},\n";
+                // Validate that we sample time values in the range [lo, hi].
+                LemonAssert.IsEqual(lo, this.TimesSampled[0]);
+                LemonAssert.IsEqual(hi, this.TimesSampled[this.TimesSampled.Length - 1]);
             }
-            s += "}";
-            return s;
         }
+    }
 
-        public static string json(float[] values)
+    [System.Serializable]
+    public struct DumpWave
+    {
+        public string Name;
+        public DumpCurve NumTrolls;
+        public DumpCurve NumToughTrolls;
+        public DumpCurve NumBosses;
+        public DumpCurve NumKillerBosses;
+        public DumpCurve NumOgres;
+        public DumpCurve NumSquids;
+
+        public DumpWave(Wave wave)
         {
-            string s = "[";
-            for (int i = 0; i < values.Length; i++)
-            {
-                s += values[i].ToString() + ( i < (values.Length - 1) ? "," : "" );
-            }
-            s += "]";
-            return s;
+            this.Name = wave.name;
+            this.NumTrolls = new DumpCurve(wave.numTrolls);
+            this.NumToughTrolls = new DumpCurve(wave.numToughTrolls);
+            this.NumBosses = new DumpCurve(wave.numBosses);
+            this.NumKillerBosses = new DumpCurve(wave.numKillerBosses);
+            this.NumOgres = new DumpCurve(wave.numOgres);
+            this.NumSquids = new DumpCurve(wave.numSquids);
         }
     }
 }
--- KCUI/KCUI.cs
+++ KCUI/KCUI.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq.Expressions;
-using System.Net;
+using System.Linq;
 using MelonLoader;
+using MelonLoader.Assertions;
+using Newtonsoft.Json;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace KCUI
 {
@@ -12,23 +13,38 @@ namespace KCUI
     {
         public override void OnApplicationStart()
         {
+            LoggerInstance.Msg("Initialization started ...");
+
+            // Validate dependencies manually as a form of documentation.
+            LoggerInstance.Msg("Checking dependencies ...");
+            this.AssertFileExists(this._DEP_NEWTON);
+            this.AssertFileExists(this._DEP_SYSDAT);
+
+            // Setup for dumping Kingdom runtime game data to disk.
             if (this._FLAG_DUMP_DATA)
             {
                 LoggerInstance.Msg("Initializing dump directory ...");
-                Directory.CreateDirectory(this.dumpPath());
+                Directory.CreateDirectory(this._OUT_PATH);
             }
+
+            // Initialization finished.
+            this._initialized = true;
+            LoggerInstance.Msg("Initialization finished!");
         }
 
         public override void OnUpdate()
         {
+            // Silently do nothing if mod initialization failed.
+            if (!this._initialized) return;
+
+            /* Note: OnSceneWasLoaded is compatible with Unity v5.4+ but we target Unity v5.3.4!
+                For MelonLoader OnLevelWasLoaded is deprecated and now subscribed to OnSceneWasLoaded
+                instead! */
             var activeScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
             if (activeScene.name == this._SCENE_MAIN && this._FLAG_DUMP_DATA)
             {
-                /* Note: OnSceneWasLoaded is compatible with Unity v5.4+ but we target Unity v5.3.4!
-                    For MelonLoader OnLevelWasLoaded is deprecated and now subscribed to OnSceneWasLoaded
-                    instead! */
-                this.dumpDays();      
-                this.dumpWaves();
+                this.DumpDays();      
+                this.DumpWaves();
                 // CLeanup.
                 this._FLAG_DUMP_DATA = false;
                 
@@ -36,65 +52,67 @@ namespace KCUI
             }
         }
 
-        /* Get the path this mod dumps non-MelonLoader output files to. */
-        public string dumpPath()
+        /* Assert that the given file path dependency exists. */
+        public void AssertFileExists(string path)
         {
-            return $"Mods/{nameof(KCUI)}";
+            LemonAssert.IsTrue(File.Exists(path), $"Missing dependency: {path}");
         }
         
         /* Dump the regular, boss and recovery days lists of the Director in human-readable format. */
-        public void dumpDays()
+        public void DumpDays()
         {
             try
             {
-                string s = "{\n";
-                s += JSON.json(Managers.director.regularDays)  + "\n";
-                s += JSON.json(Managers.director.bossDays)     + "\n";
-                s += JSON.json(Managers.director.recoveryDays) + "\n";
-                s += "}";
-
-                string dumpPath = $"{this.dumpPath()}/days.json";
-                using (StreamWriter writer = new StreamWriter(dumpPath))
+                IEnumerable<Day> days = Managers.director.regularDays.
+                    Concat(Managers.director.bossDays).
+                    Concat(Managers.director.recoveryDays);
+                foreach (Day day in days)
                 {
-                    writer.Write(s);
-                    LoggerInstance.Msg($"Succeeded to dump Day data: {dumpPath}");
+                    string dumpPath = $"{this._OUT_PATH}/day-{day.name}.json";
+                    using (StreamWriter writer = new StreamWriter(dumpPath))
+                    {
+                        writer.Write(JsonConvert.SerializeObject(new DumpDay(day), Formatting.Indented));
+                        LoggerInstance.Msg($"Succeeded to dump Day data: {dumpPath}");
+                    }
                 }
             }
             catch (Exception e)
             {
                 LoggerInstance.Msg($"Failed to dump Day data: {e.Message}");                
+                throw e;
             }
         }
 
         /* Dump the different enemy Wave types in human-readable format. */
-        public void dumpWaves()
+        public void DumpWaves()
         {
             try
             {
-                string s = "{\n";
                 string RES_PATH_WAVES = "Waves"; // The Waves resource path.
                 foreach (Wave wave in Resources.LoadAll<Wave>(RES_PATH_WAVES))
                 {
-                    s += JSON.json(wave) + ",\n";
-                }
-                s += "}";
-
-                string dumpPath = $"{this.dumpPath()}/waves.json";
-                using (StreamWriter writer = new StreamWriter(dumpPath))
-                {
-                    writer.Write(s);
-                    LoggerInstance.Msg($"Succeeded to dump Wave data: {dumpPath}");
+                    string dumpPath = $"{this._OUT_PATH}/wave-{wave.name}.json";
+                    using (StreamWriter writer = new StreamWriter(dumpPath))
+                    {
+                        writer.Write(JsonConvert.SerializeObject(new DumpWave(wave), Formatting.Indented));
+                        LoggerInstance.Msg($"Succeeded to dump Wave data: {dumpPath}");
+                    }
                 }
             }
             catch (Exception e)
             {
                 LoggerInstance.Msg($"Failed to dump Wave data: {e.Message}");
+                throw e;
             }
         }
 
-        /* The name constants of Unity scenes. */
-        private string _SCENE_MAIN = "main"; // The main scene name.
-        /* Boolean flags of the mod. */
+        /* String constants. */
+        private string _OUT_PATH { get => $"Mods/{nameof(KCUI)}"; } // The path to dump output files to.
+        private string _SCENE_MAIN = "main"; // The Unity main scene name.
+        private string _DEP_NEWTON = "UserLibs/Newtonsoft.Json.dll"; // Newtonsoft.Json dependency path.
+        private string _DEP_SYSDAT = "UserLibs/System.Data.dll"; // System.Data dependency path.
+        /* Boolean flags. */
+        private bool _initialized = false;
         private bool _FLAG_DUMP_DATA = true;
     }
 }
